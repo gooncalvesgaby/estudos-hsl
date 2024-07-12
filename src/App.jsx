321c2871bf20b51e6367f096fb8d959bd5d92075
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-07-12 10:26:00 -0300

    criação de uma nova branch e componentização

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from "react"
+import {createGlobalStyle} from "styled-components"
+import Button from "./Components/Button/button.jsx"
+
+const GlobalStyle = createGlobalStyle`
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+`
 
 function App() {
   return (
     <>
-      
+    <GlobalStyle/>
+    <Button/>
     </>
   )
 }
 
 export default App
+
---
+++ src/Components/Button/button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import * as S from "./buttonStyle.jsx"
+
+function Button(){
+    return(
+        <>
+        </>
+    )
+}
+
+export default Button
