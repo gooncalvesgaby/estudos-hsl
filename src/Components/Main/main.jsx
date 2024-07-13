f396646a2909b78e7d7f15305cb1a992cd119a92
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-07-13 13:09:58 -0300

    ajustes na tela da main

--- src/Components/Main/main.jsx
+++ src/Components/Main/main.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import * as S from "./mainStyle.jsx";
+import Dive from "../assets/dive.png"
+import Virtual from "../assets/virtual.png"
+import Arrow from "../assets/arrow.png"
+import Lorem from "../assets/lorem.png"
+import Pexels from "../assets/pexels.png"
+import Button from "../Button/button.jsx"
 
-function Main(){
-    return(
+
+function Main() {
+    return (
         <S.Main>
-
+            <S.Div>
+                <S.Div2>
+                    <S.Img src={Dive} alt="parágrafo de titulo" />
+                    <S.Img src={Virtual} alt="parágrafo de titulo" />
+                    <S.Img src={Lorem} alt="parágrafo de texto" />
+                </S.Div2>
+                <S.Div3>
+                    <Button text="BUILD YOUR WORLD" />
+                    <S.Img2 src={Arrow} alt="seta para a direita" />
+                </S.Div3>
+                <S.Div4>
+                    <S.Img3 src={Pexels} alt="mulher com óculos" />
+                </S.Div4>
+            </S.Div>
         </S.Main>
     )
 }
--- src/Components/Main/mainStyle.jsx
+++ src/Components/Main/mainStyle.jsx
@@ -1,5 +1,32 @@
 import styled from "styled-components";
 
 export const Main = styled.main`
+background-color:  #302C42;
+height: 60vh;
+`
 
+export const Div = styled.div`
+
+`
+export const Div2 = styled.div`
+display: grid;
+`
+
+export const Div3 = styled.div`
+display: flex;
+align-items: center;
+`
+
+export const Img = styled.img`
+width: 22vw;
+`
+
+export const Img2 = styled.img`
+`
+
+export const Div4 = styled.div`
+
+`
+export const Img3 = styled.img`
+width: 25vw;
 `
